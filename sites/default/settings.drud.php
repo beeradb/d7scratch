500a48eb5c25d9ab2ff47d180005734831460319
Brad Bowman <beeradb@gmail.com>
2018-03-05 16:47:36 -0500

    [master] Add block_cache

--- sites/default/settings.drud.php
+++ sites/default/settings.drud.php
@@ -3,5 +3,6 @@
 $conf['cache_backends'][] = 'sites/all/modules/varnish/varnish.cache.inc';
 $conf['cache_class_cache_page'] = 'VarnishCache';
 $conf['cache'] = 1;
+$conf['block_cache'] = 1;
 $conf['cache_lifetime'] = 60;
 $conf['page_cache_maximum_age'] = 0;
